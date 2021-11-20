d702672330bab440583ccc692d4ab9623d49694c
vinsdragonis <zrexteam128@gmail.com>
2021-11-21 02:14:47 +0530

    Added navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import io from 'socket.io-client';
 import { useState } from 'react';
 import Chat from './components/Chat.js';
+import Navbar from './components/Navbar';
 
 const socket = io.connect("http://localhost:3001");
 
@@ -19,6 +20,7 @@ function App() {
   
   return (
     <div className="App">
+      <Navbar />
       { !showChat ? (
         <div className="joinChatContainer shadow-5 tc">
           <h3>Join a chat</h3>
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Navbar = () => {
+  return (
+    <nav style={{display: "flex", justifyContent: "space-between"}}>
+      <div className="tr" style={{display: "flex", justifyContent: "space-between"}}>
+        <p className="f2 black dim b underline-hover pa2 br3 pointer" style={{paddingLeft: "20px"}}>DRAKESPEARE</p>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
