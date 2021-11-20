73ea8f45d30ec58d602b24eb43975bada998f8c1
vinsdragonis <zrexteam128@gmail.com>
2021-11-20 22:59:42 +0530

    Setup socket for server (minimal)

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
-// import './App.css';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -7,6 +7,20 @@ const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  });
+});
 
 server.listen(3001, () => {
   console.log('Server started listening on port 3001');
