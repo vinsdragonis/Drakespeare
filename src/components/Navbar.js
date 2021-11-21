f2e0bd188bedc282fc0121270f88b00e18ce7402
vinsdragonis <zrexteam128@gmail.com>
2021-11-22 00:56:18 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav style={{display: "flex", justifyContent: "space-between"}}>
       <div className="tl">
-        <h1 className="f2 black dim b underline-hover pa2 br3 pointer" style={{paddingLeft: "20px"}}>DRAKESPEARE</h1>
+        <h1 className="f2 white dim b underline-hover pa2 br3 pointer" style={{paddingLeft: "20px"}}>DRAKESPEARE</h1>
       </div>
     </nav>
   );
