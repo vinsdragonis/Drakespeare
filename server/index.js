cd41b0f3cebed106a99d3382cf7ce8471ebbcab0
vinsdragonis <zrexteam128@gmail.com>
2021-11-21 21:09:57 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://drakespeare.herokuapp.com/",
+    origin: "https://drakespeare.netlify.app/",
     methods: ["GET", "POST"]
   }
 });
