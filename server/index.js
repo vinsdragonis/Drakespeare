4b758935d01b13efcbbf48306e0972d799d70839
vinsdragonis <zrexteam128@gmail.com>
2021-11-21 20:48:10 +0530

    .

--- server/index.js
+++ server/index.js
@@ -12,23 +12,23 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://github.com/vinsdragonis/Drakespeare/tree/main/server",
     methods: ["GET", "POST"]
   }
 });
 
 io.on("connection", (socket) => {
   console.log(`User connected: ${ socket.id }`);
-  
+
   socket.on("join_room", (data) => {
     socket.join(data);
     console.log(`User with ID: ${ socket.id } joined room: ${ data }`);
   })
-  
+
   socket.on("send_message", (data) => {
     socket.to(data.room).emit("receive_message", data);
   })
-  
+
   socket.on("disconnect", () => {
     console.log("User disconnected: ", socket.id);
   })
