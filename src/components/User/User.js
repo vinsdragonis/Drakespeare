bf31ebc7ced06d802bac9bad3d1b08bbc52ebd99
vinsdragonis <zrexteam128@gmail.com>
2021-11-25 17:40:35 +0530

    .

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -6,7 +6,7 @@ function User({ socket, username, room }) {
       <div
         className="userDetails"
         style={{
-          marginTop:"10px",
+          marginTop:"0",
           paddingLeft:"15px",
           paddingBottom:"25px",
           width:"500px"
