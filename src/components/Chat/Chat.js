e532d8fe2fea3c1428589583218beb41e46c0b63
vinsdragonis <zrexteam128@gmail.com>
2021-11-27 01:34:49 +0530

    Update Chat.js

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -5,16 +5,23 @@ function Chat({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setmessageList] = useState([]);
 
+  const getCurrentTime = () => {
+    var currentHours = new Date(Date.now()).getHours();
+    var currentMinutes = new Date(Date.now()).getMinutes();
+
+    currentHours = ("0" + currentHours).slice(-2);
+    currentMinutes = ("0" + currentMinutes).slice(-2);
+
+    return currentHours + ":" + currentMinutes;
+  }
+
   const sendMessage = async () => {
     if (currentMessage !== "") {
       const messageData = {
         room: room,
         author: username,
         message: currentMessage,
-        time:
-          new Date(Date.now()).getHours() +
-          ":" +
-          new Date(Date.now()).getMinutes(),
+        time: getCurrentTime()
       }
 
       await socket.emit("send_message", messageData);
